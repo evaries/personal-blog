def71a700f89dffb35e5fb66c8da6c94bb9ea37d
Evgeniy Nadtochiy <e.nadtochiy@MacBook-Air-Evgeniy.local>
2021-01-10 18:43:13 +0200

    add google a

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,6 +59,15 @@ module.exports = {
         head: true,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-4C7G1FCJ40", // Google Analytics / GA
+        ],
+      },
+    },
     `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
